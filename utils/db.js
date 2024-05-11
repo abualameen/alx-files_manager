72e123cde02009097b5bf7b79ac86905ff41815e
abualameen <abulyaqs@gmail.com>
2024-05-11 07:30:48 -0700

    nowww

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,5 @@
 import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 class DBClient {
     constructor(host = process.env.DB_HOST || 'localhost', port = process.env.DB_PORT || 27017, dbName = process.env.DB_DATABASE || 'files_manager') {
