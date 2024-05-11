a2cbe923106fde68d6091fb4f9e51d53fe3c7bfd
abualameen <abulyaqs@gmail.com>
2024-05-11 20:47:41 +0100

    taask 2

---
+++ controllers/AppController.js
@@ -0,0 +1,36 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const AppController = {
+    getStatus: async(res) => {
+        try {
+            const isRedisAlive = await redisClient.isAlive();
+            const isDBAlive = await dbClient.isAlive();
+
+            if (isRedisAlive && isDBAlive) {
+                res.status(200).send({ "redis": true, "db": true });
+            } else {
+                res.status(500).send('Internal server Error');
+            }
+        } catch (error) {
+            console.error('Error checking status:', error);
+            res.status(500).send('Internal Server Error');
+        }
+  
+    },
+    getStats: async(res) => {
+        if  (dbClient.isAlive()) {
+            try {
+                const usersCount = await dbClient.nbUsers();
+                const filesCount = await dbClient.nbFiles();
+                res.send({ users: usersCount, files: filesCount });
+            } catch (error) {
+                console.error('Error fetching stats:', error);
+                res.status(500).send('Internal Server Error');
+            }
+
+        }
+    }
+};
+
+module.exports = AppController;
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes/index');
+
+const PORT = process.env.PORT || 5000;
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
