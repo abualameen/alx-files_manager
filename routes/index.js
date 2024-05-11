991bd1e4c4088ac8b8f8214abd1aabe34b67e17f
abualameen <abulyaqs@gmail.com>
2024-05-11 21:09:03 +0100

    taskk2

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const AppController = require('../controllers/AppController');
+
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
