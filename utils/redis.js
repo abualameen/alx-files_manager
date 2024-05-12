cdbe40afb498836f7bd93e501a92104f7983e17b
abualameen <abulyaqs@gmail.com>
2024-05-12 09:37:08 +0100

    taskk2

--- utils/redis.js
+++ utils/redis.js
@@ -5,52 +5,20 @@ class RedisClient {
     this.client = redis.createClient({
       host: 'localhost',
       port: 6379,
+      debug_mode: true,
     });
     this.isConnected = false;
+
     this.client.on('error', (error) => {
-      console.error('Redis Client Error:', error);
+      console.error('Redis Client Eroor:', error);
     });
     this.client.on('connect', () => {
       this.isConnected = true;
-    });
-  }
-
-  async connect() {
-    try {
-      // No need to explicitly connect, as it's done internally by the client
-      this.isConnected = true;
-    } catch (error) {
-      console.error('Error connecting to Redis:', error);
-    }
+    })
   }
 
   async isAlive() {
-    await this.waitConnection();
     return this.isConnected;
-  }
-
-  waitConnection() {
-    return new Promise((resolve, reject) => {
-      if (this.isConnected) {
-        resolve();
-      } else {
-        const timeout = setTimeout(() => {
-          clearTimeout(timeout);
-          reject(new Error('Connection timeout'));
-        }, 10000); // Adjust the timeout as needed
-
-        const checkConnection = () => {
-          if (this.isConnected) {
-            clearTimeout(timeout);
-            resolve();
-          } else {
-            setTimeout(checkConnection, 100); // Check again after a short delay
-          }
-        };
-
-        checkConnection();
-      }
-    });
   }
 
   async get(key) {
@@ -91,5 +59,4 @@ class RedisClient {
 }
 
 const redisClient = new RedisClient();
-// No need to call redisClient.connect() explicitly
 module.exports = redisClient;
