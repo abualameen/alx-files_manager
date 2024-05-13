4421bffed3fb1f003b882d8de6711808d1647fd9
abualameen <abulyaqs@gmail.com>
2024-05-13 09:21:06 +0100

    resoving2

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -29,7 +29,7 @@ const UsersController = {
     },
 
     getMe: async (req, res) => {
-        const token = req.headers['x-token'];
+        const token = req.headers['X-Token'];
         if (!token) {
         return res.status(401).json({ error: 'Unauthorized' });
         }
