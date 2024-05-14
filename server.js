1b5bfc0fc1371de2fa12a44a0d8b049ad60910c6
abualameen <abulyaqs@gmail.com>
2024-05-14 21:50:41 +0100

    all remaining task

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const routes = require('./routes/index');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.use(bodyParser.json());
 app.use('/', routes);
-app.use(bodyParser.json({ limit: '20mb' }));
-app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
