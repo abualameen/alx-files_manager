36b2cd399e3d735f7ecdbca51b9372f47b20524b
abualameen <abulyaqs@gmail.com>
2024-05-14 20:55:03 +0100

    all remaining task

--- image_upload.py
+++ image_upload.py
@@ -9,9 +9,17 @@ file_encoded = None
 with open(file_path, "rb") as image_file:
     file_encoded = base64.b64encode(image_file.read()).decode('utf-8')
 
-r_json = { 'name': file_name, 'type': 'image', 'isPublic': True, 'data': file_encoded, 'parentId': sys.argv[3] }
-r_headers = { 'X-Token': sys.argv[2] }
+# r_json = { 'name': image.png, 'type': 'image', 'isPublic': True, 'data': file_encoded, 'parentId': sys.argv[3] }
+# r_headers = { 'X-Token': sys.argv[2] }
+import requests
+import json
 
-r = requests.post("http://0.0.0.0:5000/files", json=r_json, headers=r_headers)
-print('rrr', r.text)
+headers = {'Content-Type': 'application/json'}
+payload = {'name': 'image.png', 'type': 'image', 'isPublic': True, 'data': 'base64_encoded_data', 'parentId': 'parent_id'}
+
+response = requests.post('http://0.0.0.0:5000/files', headers=headers, data=json.dumps(payload))
+
+
+# r = requests.post("http://0.0.0.0:5000/files", json=r_json, headers=r_headers)
+# print('rrr', r.text)
 print(r.json())
