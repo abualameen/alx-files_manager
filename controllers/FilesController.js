fca6515a8a045fd05e36d8bf7e28ae8ffb775b47
abualameen <abulyaqs@gmail.com>
2024-05-14 13:19:52 +0100

    all remaining task

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -5,7 +5,7 @@ const mime = require('mime-types');
 const Bull = require('bull');
 // const imageThumbnail = require('image-thumbnail');
 const dbClient = require('../utils/db');
-const dbClient = require('../utils/redis');
+const redisClient = require('../utils/redis');
 
 // Bull queue
 const fileQueue = new Bull('fileQueue');
