3452fc58ceded9ac8a6cdb3b840a6984ba8d3ac6
abualameen <abulyaqs@gmail.com>
2024-05-15 13:38:14 +0100

    all remaining task

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
-const routes = require('./routes');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(routes);
+app.use(bodyParser.json());
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
