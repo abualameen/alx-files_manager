b9483727e239dbf3b8befd2a9e5b99e940b0e438
abualameen <abulyaqs@gmail.com>
2024-05-15 12:47:51 +0100

    all remaining task

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,4 @@ router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 router.get('/files/:id/data', FilesController.getFile);
 
-module.exports = router;
+export default router;
