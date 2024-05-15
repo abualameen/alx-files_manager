a78d1ecbb3d6881acfb338b029b34da8c977390a
abualameen <abulyaqs@gmail.com>
2024-05-15 20:29:16 +0100

    all remaining task

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,33 +3,18 @@ import dbClient from '../utils/db';
 
 const AppController = {
   getStatus: async (req, res) => {
-    try {
-      const isRedisAlive = redisClient.isAlive();
-      const isDBAlive = dbClient.isAlive();
 
-      if (isRedisAlive && isDBAlive) {
-        return res.status(200).json({ redis: isRedisAlive, db: isDBAlive });
-      } else {
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-    } catch (error) {
-      console.error('Error checking status:', error);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
+    const isRedisAlive = redisClient.isAlive();
+    const isDBAlive = dbClient.isAlive();
+
+    return res.status(200).json({ redis: isRedisAlive, db: isDBAlive });
   },
   getStats: async (req, res) => {
-    try {
-      if (dbClient.isAlive()) {
-        const usersCount = await dbClient.nbUsers();
-        const filesCount = await dbClient.nbFiles();
-        return res.status(200).json({ users: usersCount, files: filesCount });
-      } else {
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-    } catch (error) {
-      console.error('Error fetching stats:', error);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
+   
+    
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    return res.status(200).json({ users: usersCount, files: filesCount });
   },
 };
 
