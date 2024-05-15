dbdc34f9716c43d68fa7fe501dfb9936a7c021a7
abualameen <abulyaqs@gmail.com>
2024-05-15 13:34:22 +0100

    all remaining task

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', routes);
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
